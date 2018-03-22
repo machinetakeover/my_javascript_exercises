0ff575d47ccdefca996615964316ed01fefb2687
machinetakeover <37376960+machinetakeover@users.noreply.github.com>
2018-03-22 12:42:16 -0400

    new interview questions

---
+++ _interview_questions.js
@@ -0,0 +1,14 @@
+// Write a function that multiply 2 numbers
+
+
+// multiply(3, 5) //should return 15
+
+
+// Write a function that loops through the array and returns all even numbers
+let array1 = [2, 5, 13, 8, 38, 55];
+
+function findEven(arr) {
+
+}
+
+// findEven(array1); //should return 2, 8, 38
